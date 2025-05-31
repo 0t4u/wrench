67404d188aaae98b99e12d05c413ea029516cb45
0t4u <61939142+0t4u@users.noreply.github.com>
2025-05-31 14:23:51 +0100

    chore: sync with fix branch

--- internal/wrench/http_pkg.go
+++ internal/wrench/http_pkg.go
@@ -282,9 +282,14 @@ func (b *Bot) httpPkgEnsureZigVersionCached(version, versionKind string) {
 	}
 }
 
+type cachedResponse struct {
+	err    error
+	expire int64 // seconds
+}
+
 var (
 	cachedResponsesMu sync.Mutex
-	cachedResponses   = map[string]error{}
+	cachedResponses   = map[string]cachedResponse{}
 	fsParallelismLock sync.Mutex
 )
 
@@ -342,8 +347,13 @@ func (b *Bot) httpPkgEnsureZigDownloadCached(version, versionKind, fname string)
 	cachedError, isCachedError := cachedResponses[url]
 	cachedResponsesMu.Unlock()
 	if isCachedError {
-		fmt.Fprintf(logWriter, "not fetching: %s (cached error %s)\n", url, cachedError)
-		return cachedError
+		if cachedError.expire >= time.Now().Unix() {
+			delete(cachedResponses, url)
+			fmt.Fprintf(logWriter, "deleted cached error for %s (cached error %s)\n", url, cachedError.err)
+		} else {
+			fmt.Fprintf(logWriter, "not fetching: %s (cached error %s)\n", url, cachedError.err)
+			return cachedError.err
+		}
 	}
 	fmt.Fprintf(logWriter, "fetch: %s > %s\n", url, filePath)
 
@@ -359,29 +369,30 @@ func (b *Bot) httpPkgEnsureZigDownloadCached(version, versionKind, fname string)
 		// 404 not found, 403 forbidden, etc.
 		err := fmt.Errorf("bad response status: %s", resp.Status)
 
-		// file not available from source (but we expect it to be reachable)
-		// we should not encounter 429 status codes here, so we don't handle it
-		// maybe we should periodically clear the cache or use a LRU cache instead of doing this
-		// but we might accidentally DoS the source if unavailable files are continuously being
-		// requested (at that point why aren't you imposing ratelimits on spammy traffic???)
+		// no expiry by default, cache forever
+		var expiry = int64(0)
+
+		// file not available from upstream (but we expect it to be reachable)
+		// does not consider how to handle 429s from upstream
 		if (resp.StatusCode == 404 || resp.StatusCode >= 500) && versionKind == "stable" {
 			versionParts := strings.Split(version, ".")
 
-			// fail silently, this is not critical
+			// fail silently, this is probably fine
 			if i, e := strconv.ParseInt(versionParts[1], 10, 64); e == nil {
 				// versions below 0.5.0 are allowed to be unavailable
 				if !(versionParts[0] == "0" && i <= 5) {
-					// don't cache error, we expect it to be temporary (we might need to cache 500
-					// status codes to prevent accidentally DoSing the source, to be decided)
-					fmt.Fprintf(logWriter, "error not cached: unexpected %v response for files with release %s", resp.StatusCode, version)
-					return err
+					// cache for 5 mins, we expect it to be reachable later
+					expiry = time.Now().Unix() + (60 * 5)
+					fmt.Fprintf(logWriter, "error cached with expiry: unexpected %v response for files with release %s", resp.StatusCode, version)
 				}
-
 			}
 		}
 
 		cachedResponsesMu.Lock()
-		cachedResponses[url] = err
+		cachedResponses[url] = cachedResponse{
+			err:    err,
+			expire: expiry,
+		}
 		cachedResponsesMu.Unlock()
 		return err
 	}
